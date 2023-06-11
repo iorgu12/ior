0185348b4f4e458385fd6f5fc4f200f2ec2536a2
iorgu12 <74180304+iorgu12@users.noreply.github.com>
2023-06-12 01:00:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify, request
+
 app = Flask(__name__)
+
 @app.route('/api')
 def hello():
-return jsonify(
-name="Hello",
-description="World",
-Url=request.url
-)
-return 'Hello, World!'
+    return jsonify(
+        name="Hello",
+        description="World",
+        Url=request.url
+    )
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
 if __name__ == '__main__':
-app.run(host="0.0.0.0", port=4444, debug=True)
+    app.run(host="0.0.0.0", port=4444, debug=True)

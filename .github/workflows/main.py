44d761548c71904b3707c75f5abef1e5baeb8aa6
iorgu12 <74180304+iorgu12@users.noreply.github.com>
2023-06-11 19:35:25 +0200

    Create main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+@app.route('/api')
+def hello():
+return jsonify(
+name="Hello",
+description="World",
+Url=request.url
+)
+return 'Hello, World!'
+if __name__ == '__main__':
+app.run(host="0.0.0.0", port=4444, debug=True)
